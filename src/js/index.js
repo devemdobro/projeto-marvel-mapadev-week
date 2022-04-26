0e7b52a2e999048f551ccfb757b51ecd19292f61
Roberto Dias <roberto.rhd@gmail.com>
2022-04-26 12:04:52 -0300

    melhora nomes de classes e ids

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,17 @@ const personagens = document.querySelectorAll(".personagem");
 
 personagens.forEach((personagem) => {
   personagem.addEventListener("mouseenter", () => {
-
     const idSelecionado = personagem.attributes.id.value;
 
-    if(idSelecionado === 'ultron') return;
-    
-    const nomeSelecionado = personagem.getAttribute('data-name');
+    if (idSelecionado === "ultron") return;
 
-    const imagemJogador1 = document.getElementById("imagem-player-1");
+    const nomeSelecionado = personagem.getAttribute("data-name");
+
+    const imagemJogador1 = document.getElementById("personagem-jogador-1");
     imagemJogador1.src = `./src/imagens/${idSelecionado}.png`;
 
-    const nomeJogador1Personagem = document.querySelector('.personagem-grande.player-1')
-    nomeJogador1Personagem.getElementsByTagName('h2')[0].innerHTML = nomeSelecionado
+    const personagemJogador1 = document.querySelector(".personagem-grande.personagem-jogador-1");
+    personagemJogador1.getElementsByTagName("h2")[0].innerHTML = nomeSelecionado;
 
     const personagemSelecionado = document.querySelector(".selecionado");
     personagemSelecionado.classList.remove("selecionado");
